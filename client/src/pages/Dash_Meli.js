9da77d0516a8e1a0f873898dd951ef200db5f4fa
CarlosDev0410 <89785602+CarlosDev0410@users.noreply.github.com>
2024-08-05 23:49:01 -0300

    fim da estrutura

---
+++ client/src/pages/Dash_Meli.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dash_Meli = () => {
+  return (
+    <div>
+      <h1>Bem-vindo ao Dashboard</h1>
+      {/* Adicione componentes de visualização de dados aqui */}
+    </div>
+  );
+};
+
+export default Dash_Meli;
