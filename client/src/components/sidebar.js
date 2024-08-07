99c71a6365690e5a5bd4fdfe7e326e40b9a8d51d
CarlosDev0410 <89785602+CarlosDev0410@users.noreply.github.com>
2024-08-06 23:58:29 -0300

    Commit

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './Sidebar.css';
 import logo from '../assets/logo-temperare.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrophy, faMoneyBill, faBoxOpen, faArrowLeft, faArrowRight, faHandshake} from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -10,20 +12,32 @@ const Sidebar = () => {
   };
 
   return (
-    <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
-      <div className="sidebar-logo">
-        <img src={logo} alt="Logo" />
+      <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
+        <div className="sidebar-logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="sidebar-menu">
+          <div className="menu-item">
+              <FontAwesomeIcon icon={faTrophy} />
+              {!isCollapsed && <span>Reputação</span>}
+          </div>
+          <div className="menu-item">
+            <FontAwesomeIcon icon={faMoneyBill} />
+            {!isCollapsed && <span>Financeiro</span>}
+          </div>
+          <div className="menu-item">
+            <FontAwesomeIcon icon={faHandshake} />
+            {!isCollapsed && <span>Vendas</span>}
+          </div>
+          <div className="menu-item">
+            <FontAwesomeIcon icon={faBoxOpen} />
+            {!isCollapsed && <span>Logística</span>}
+          </div>
+        </div>
+        <div className="sidebar-toggle-indicator" onClick={toggleSidebar}>
+          {isCollapsed ? {faArrowRight} : {faArrowLeft}}
+        </div>
       </div>
-      <div className="sidebar-menu">
-        <button className="sidebar-button">Faturamento</button>
-        <button className="sidebar-button">Reputação</button>
-        <button className="sidebar-button">Finanças</button>
-        <button className="sidebar-button">Estoque</button>
-      </div>
-      <div className="sidebar-toggle-indicator" onClick={toggleSidebar}>
-        {isCollapsed ? '>>' : '<<'}
-      </div>
-    </div>
   );
 };
 
